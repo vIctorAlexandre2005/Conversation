ef6fe99c4e96255b8b3e892edecc3c796225d4b5
Victor Alexandre <vtrm2005@gmail.com>
2024-05-13 16:59:21 -0300

    Renoved

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e8985825d3ea68cc"; }
+/******/ 		__webpack_require__.h = function() { return "75175354acd2142c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -18,3 +18,23 @@ const auth = app.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
 
 export { db, auth, provider };
+
+/* 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCDZ4StME58AH39v1-3vzqvf2v__MuPHr8",
+  authDomain: "chatnovo-78e11.firebaseapp.com",
+  projectId: "chatnovo-78e11",
+  storageBucket: "chatnovo-78e11.appspot.com",
+  messagingSenderId: "243060174224",
+  appId: "1:243060174224:web:d2e5c59b62b11e48c65715"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
